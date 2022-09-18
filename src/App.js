ecb174221477dc4b3061df765e784122829e0d4e
sabriyeuregen <sabriyeuregen@gmail.com>
2022-09-18 14:47:58 +0300

    Added: dark and light mode

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import UserForm from "./components/User/UserForm";
 import Create from "./components/Create/Create";
 import Read from './components/Read/Read';
 import Update from './components/Update/Update';
+import useLocalStorage from 'use-local-storage'
+import  "./App.css";
 
  const App = () => {
+
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+ const switchTheme = () => {
+   const newTheme= theme=== "light" ?  "dark" : "light";
+   setTheme(newTheme)
+
+ }
+
+
+
   return (
-    <div className="App">
+    <div className="App" data-theme={theme}>
+      <button onClick={switchTheme}>
+       Select To {theme === "light" ? "Dark" : "Light"} Theme
+      </button>
       <UserForm></UserForm>
       <Create></Create>
       <Update></Update>
